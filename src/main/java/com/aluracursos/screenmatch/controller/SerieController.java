ec01566814c9ad1c05fa57eaff40593040c36fb7
Sam Rosas <sam78roga@gmail.com>
2025-08-19 18:29:45 -0600

    Subiendo repositorio

---
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -0,0 +1,40 @@
+package com.aluracursos.screenmatch.controller;
+
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.service.SeriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/series")
+public class SerieController {
+    @Autowired
+    private SeriesService servicio;
+
+
+    @GetMapping()
+    public List<SerieDTO> obtenerTodasLasSeries(){
+        return servicio.obtenerTodasLasSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obtenerTop5(){
+        return servicio.obtenerTop5();
+    }
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> obtenerLanzamientosMasRecientes(){
+        return servicio.obtenerLanzamientosMasRecientes();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obtenerPorId(@PathVariable Long id){
+        return servicio.obtenerPorId(id);
+    }
+
+
+}
